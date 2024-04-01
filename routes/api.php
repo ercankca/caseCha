90d3ad890253f866dc52e23b2ae995a17f4dc9cd
Ercan K <ercankca@gmail.com>
2024-04-01 10:59:46 +0300

    api eklendi

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubscriptionController;
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('/subscriptions', [SubscriptionController::class, 'index']);
+    Route::post('/subscriptions', [SubscriptionController::class, 'store']);
+    Route::get('/subscriptions/{id}', [SubscriptionController::class, 'show']);
+    Route::put('/subscriptions/{id}', [SubscriptionController::class, 'update']);
+    Route::delete('/subscriptions/{id}', [SubscriptionController::class, 'destroy']);
+});
